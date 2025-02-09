281eafd2aaacfa0eb0d6ce811d5e4f446cc1a127
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-09 10:58:12 -0700

    Import script starter, add dummy code for HTML/CSS

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Test external link
+console.log("A Library Project? Here?! Are you mad?!");
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+};
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+};
