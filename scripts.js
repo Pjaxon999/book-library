82bf8f343b426436219d2642c776b90d70f386f9
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-09 11:42:14 -0700

    Add book constructor, array modifier, HTML outline
    The basic way I see this working is by outputting the contents of the
    Library Array into the table. The script currently does not create an
    array of objects but instead an array of the information contained
    within the objects as they are created, so that may need to change.

--- scripts.js
+++ scripts.js
@@ -3,10 +3,24 @@ console.log("A Library Project? Here?! Are you mad?!");
 
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+    }
 };
 
 function addBookToLibrary() {
   // take params, create a book then store it in the array
+    let title = prompt("What is the name of the book?");
+    let author = prompt("Who is the author of this book?");
+    let pages = prompt("How many pages does this book have?");
+    let read = prompt("Is this a book you have read or not read?");
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook.info());
+    console.log(myLibrary);
 };
+
