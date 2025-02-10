f3538a818927cf5fca5e12b36ade23b991370eec
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2025-02-10 10:11:08 -0700

    Add Script for adding Books, adjust table layout
    The table is now laid out more sensibly with a thead and tbody. The
    script now adds a new row with the information gathered from the
    information gathered from the prompts. This of course will change to
    some kind of form, but for now I wanted to make sure the script was
    working as intended. I would like to see if there is a clean way to add
    a scope attribute to the new rows as they are added. I also need to
    personally add the scope to the headings.

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
 // Test external link
 console.log("A Library Project? Here?! Are you mad?!");
 
+// grab objects for manipulation later
+let libraryTable = document.querySelector("table");
+
+
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, read, score) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.score = score;
     this.info = function () {
         return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
     }
@@ -18,9 +23,23 @@ function addBookToLibrary() {
     let title = prompt("What is the name of the book?");
     let author = prompt("Who is the author of this book?");
     let pages = prompt("How many pages does this book have?");
-    let read = prompt("Is this a book you have read or not read?");
-    let newBook = new Book(title, author, pages, read);
+    let read = prompt("Have you read this book?");
+    let score = prompt("If you've read this book, please rate it out of 10. If you haven't read the book or don't feel like adding a score, type 'N/A'")
+    let newBook = new Book(title, author, pages, read, score);
     myLibrary.push(newBook);
-    console.log(myLibrary);
+    displayLibrary(newBook);
 };
 
+function displayLibrary(Book) {
+    let newBookRow = libraryTable.insertRow(1);
+    let titleCell = newBookRow.insertCell(0);
+    let authorCell = newBookRow.insertCell(1);
+    let pagesCell = newBookRow.insertCell(2);
+    let readCell = newBookRow.insertCell(3);
+    let scoreCell = newBookRow.insertCell(4);
+    titleCell.textContent = Book.title;
+    authorCell.textContent = Book.author;
+    pagesCell.textContent = Book.pages;
+    readCell.textContent = Book.read;
+    scoreCell.textContent = Book.score;
+}
